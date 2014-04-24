f827ee1b49f7861829e38528755e74f6f1e695f6
Andy Truong <andy@toila.net>
2014-04-24 10:08:22 +1000

    Update fn.php

--- src/fn.php
+++ src/fn.php
@@ -4,16 +4,14 @@
  * Common functions
  */
 
-namespace andytruong;
-
 /**
  * Can not:
  *    new Thing()->doStuff();
  *
  * Can:
- *    \andytruong\id(new Thing())->doStuff();
+ *    at_id(new Thing())->doStuff();
  */
-function id($x) {
+function at_id($x) {
   return $x;
 }
 
@@ -22,7 +20,7 @@ function id($x) {
  * call_user_func_array() on a class constructor, so you can instead use this
  * function:
  *
- * $obj = andytruong\newv($class_name, $argv);
+ * $obj = at_newv($class_name, $argv);
  *
  * That is, these two statements are equivalent:
  *
@@ -34,7 +32,7 @@ function id($x) {
  * @return obj     A new object of the specified class, constructed by passing
  *                  the argument vector to its constructor.
  */
-function newv($class_name, $argv = array()) {
+function at_newv($class_name, $argv = array()) {
   $reflector = new ReflectionClass($class_name);
   if ($argv) {
     return $reflector->newInstanceArgs($argv);

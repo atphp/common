ad1f3053113f99b64cf1aa90737f2ca23f5937a0
Andy Truong <andy@toila.net>
2014-05-02 14:57:58 +0700

    Using at_event_manager().

--- src/EventAware.php
+++ src/EventAware.php
@@ -10,7 +10,19 @@ use Zend\EventManager\EventManagerAwareInterface;
  * Extends this to make extensible class.
  */
 class EventAware implements EventManagerAwareInterface {
+  /**
+   * Event-manager object.
+   *
+   * @var EventManagerInterface
+   */
   protected $em;
+
+  /**
+   * Name of event-manager.
+   *
+   * @var string
+   */
+  protected $em_name;
 
   public function setEventManager(EventManagerInterface $em) {
     $em->setIdentifiers(array(__CLASS__, get_called_class()));
@@ -20,7 +32,8 @@ class EventAware implements EventManagerAwareInterface {
 
   public function getEventManager() {
     if (null === $this->em) {
-      $this->setEventManager(new EventManager());
+      $em_name = !is_null($this->em_name) ? $this->em_name : str_replace('\\', '.', get_called_class());
+      $this->setEventManager($em = at_event_manager($em_name));
     }
     return $this->em;
   }

dcb00ef9d46f63e2bf8aca766236348bbc0c9499
Andy Truong <andy@toila.net>
2014-05-19 05:56:51 +0700

    misc

--- src/TypedData/Plugin/String.php
+++ src/TypedData/Plugin/String.php
@@ -7,18 +7,18 @@ class String extends Base
 
     public function isEmpty()
     {
-    if (!is_null($this->input)) {
-      return $this->input === '';
+        if (!is_null($this->input)) {
+            return $this->input === '';
+        }
     }
-  }
 
     protected function validateInput(&$error = NULL)
     {
-    if (!is_string($this->input)) {
-      $error = 'Input is not a string value.';
-      return FALSE;
+        if (!is_string($this->input)) {
+            $error = 'Input is not a string value.';
+            return FALSE;
+        }
+        return parent::validateInput($error);
     }
-    return parent::validateInput($error);
-  }
 
 }

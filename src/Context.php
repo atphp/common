b147e89c1118637c3d036ad2403d60b157a442fa
Andy Truong <andy@toila.net>
2014-05-18 13:48:29 +0700

    Make Context class reusable.

--- src/Context.php
+++ src/Context.php
@@ -1,29 +1,38 @@
 <?php
+
 namespace AndyTruong\Common;
 
 /**
  * Super simple container, just informations which only available in request scope.
  */
-class Context implements \ArrayAccess {
-  private $container = array();
+class Context implements \ArrayAccess
+{
 
-  public function offsetSet($offset, $value) {
-    if (is_null($offset)) {
-        $this->container[] = $value;
-    } else {
-        $this->container[$offset] = $value;
+    protected $container = array();
+
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        }
+        else {
+            $this->container[$offset] = $value;
+        }
     }
-  }
 
-  public function offsetExists($offset) {
-    return isset($this->container[$offset]);
-  }
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
 
-  public function offsetUnset($offset) {
-    unset($this->container[$offset]);
-  }
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
 
-  public function offsetGet($offset) {
-    return isset($this->container[$offset]) ? $this->container[$offset] : null;
-  }
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
 }

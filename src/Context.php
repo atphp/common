d658539375de2397fa88c3f8a84573cbcce6399d
Andy Truong <thehongtt@gmail.com>
2014-07-07 22:07:23 +0700

    Context: Add comments.

--- src/Context.php
+++ src/Context.php
@@ -2,18 +2,31 @@
 
 namespace AndyTruong\Common;
 
+use ArrayAccess;
+
 /**
  * Super simple container, just informations which only available in request scope.
  */
-class Context implements \ArrayAccess
+class Context implements ArrayAccess
 {
 
     protected $container = array();
 
-    public function getContainer() {
+    /**
+     * Get container.
+     *
+     * @return array
+     */
+    public function getContainer()
+    {
         return $this->container;
     }
 
+    /**
+     * {inheritdoc}
+     * @param string $offset
+     * @param mixed $value
+     */
     public function offsetSet($offset, $value)
     {
         if (is_null($offset)) {
@@ -24,16 +37,30 @@ class Context implements \ArrayAccess
         }
     }
 
+    /**
+     * {inheritdoc}
+     * @param string $offset
+     * @return boolean
+     */
     public function offsetExists($offset)
     {
         return isset($this->container[$offset]);
     }
 
+    /**
+     * {inheritdoc}
+     * @param string $offset
+     */
     public function offsetUnset($offset)
     {
         unset($this->container[$offset]);
     }
 
+    /**
+     * {inheritdoc}
+     * @param string $offset
+     * @return mixed
+     */
     public function offsetGet($offset)
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;

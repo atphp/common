6d8666ca6241d1ec80cfd64e72743dcd9f5038f0
Andy Truong <thehongtt@gmail.com>
2014-07-13 04:23:18 +0700

    Test new Event.

--- tests/common/Traits/EventAwareTraitTest.php
+++ tests/common/Traits/EventAwareTraitTest.php
@@ -39,4 +39,24 @@ class EventAwareTraitTest extends TraitTestCase
         $obj->dispatch('my_event');
     }
 
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Event is executed. Class: AndyTruong\Common\Fixtures\Traits\EventAwareClass. Params: param_1, param_2
+     */
+    public function testDispatchCustomEvent()
+    {
+        $obj = new EventAwareClass();
+        $obj->getDispatcher()->addListener('my_event', function(CustomEvent $event) {
+            $event->getTarget();
+            $msg = sprintf(
+                'Event is executed. Class: %s. Params: %s'
+                , get_class($event->getTarget())
+                , implode(', ', $event->getParams())
+            );
+            throw new RuntimeException2($msg);
+        });
+        $event = new CustomEvent('event_aware_class', $obj, array('param_1', 'param_2'));
+        $obj->dispatch('my_event', $event);
+    }
+
 }

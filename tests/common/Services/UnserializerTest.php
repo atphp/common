0f8a9c0a93bbe59331e887eea29939089c58f8a3
Andy Truong <thehongtt@gmail.com>
2014-07-12 14:39:36 +0700

    Fix #23: Unserialize service: Lazy loading property.

--- tests/common/Services/UnserializerTest.php
+++ tests/common/Services/UnserializerTest.php
@@ -2,8 +2,10 @@
 
 namespace AndyTruong\Common\TestCases\Services;
 
+use AndyTruong\Common\Fixtures\Person;
 use AndyTruong\Common\Unserializer;
 use PHPUnit_Framework_TestCase;
+use Symfony\Component\Validator\Exception\RuntimeException;
 
 /**
  * @group unserializer
@@ -15,6 +17,41 @@ class UnserializerTest extends PHPUnit_Framework_TestCase
     {
         $unserializer = new Unserializer();
         $person_array = array('name' => 'James T.', 'father' => array('name' => 'Andy T.'));
+        $person = $unserializer->fromArray($person_array, 'AndyTruong\Common\Fixtures\Person');
+        $this->assertInstanceOf('AndyTruong\Common\Fixtures\Person', $person);
+        $this->assertInstanceOf('AndyTruong\Common\Fixtures\Person', $person->getFather());
+        $this->assertEquals('James T.', $person->getName());
+        $this->assertEquals('Andy T.', $person->getFather()->getName());
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage AndyTruong\Common\Fixtures\Person.country is not writable.
+     */
+    public function testWrongCase()
+    {
+        $unserializer = new Unserializer();
+        $person_array = array('name' => 'Matt T.', 'country' => 'Vietnam');
+        $person = $unserializer->fromArray($person_array, 'AndyTruong\Common\Fixtures\Person');
+    }
+
+    public function testAutoloading()
+    {
+        $unserializer = new Unserializer();
+
+        // Mocking
+        $em = $this->getMock('Doctrine\ORM\EntityManagerInterface');
+        $rp = $this->getMock('Doctrine\Common\Persistence\ObjectRepository');
+        $father = new Person();
+        $father->setName('Andy T.');
+
+        $em->expects($this->any())->method('getRepository')->willReturn($rp);
+        $rp->expects($this->any())->method('find')->willReturn($father);
+
+        // Inject mock to unserializer
+        $unserializer->setEntityManager($em);
+
+        $person_array = array('name' => 'James T.', 'father' => 1);
         $person = $unserializer->fromArray($person_array, 'AndyTruong\Common\Fixtures\Person');
         $this->assertInstanceOf('AndyTruong\Common\Fixtures\Person', $person);
         $this->assertInstanceOf('AndyTruong\Common\Fixtures\Person', $person->getFather());

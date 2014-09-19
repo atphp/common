a8c785cb0d820ce3abf075fda5fc742797c3e84a
Andy Truong <thehongtt@gmail.com>
2014-09-19 11:40:19 +0700

    Introduce RealObject class.

---
+++ src/RealObject.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AndyTruong\Common;
+
+use DomainException;
+use Iterator;
+
+/**
+ * Enforces stricter object semantics than PHP.
+ *
+ *  // problem
+ *  $myObj = new MyClass();
+ *  $myObj->iDontWantThis = 'too badd!';
+ *
+ *  // Fix it
+ *  class MyClass extends RealObject {}
+ *  (new MyClass)->iDontWantThis = 'throw DomainException!';
+ *
+ * The object based on this class will not allow any iterator if it's not really
+ * implemented.
+ */
+abstract class RealObject implements Iterator
+{
+
+    public function __set($name, $value)
+    {
+        throw new DomainException(sprintf(
+            'Attempt to write to undeclared property %s::%s.', get_class($this), $name
+        ));
+    }
+
+    public function current()
+    {
+        $this->throwOnAttemptedIteration();
+    }
+
+    public function key()
+    {
+        $this->throwOnAttemptedIteration();
+    }
+
+    public function next()
+    {
+        $this->throwOnAttemptedIteration();
+    }
+
+    public function rewind()
+    {
+        $this->throwOnAttemptedIteration();
+    }
+
+    public function valid()
+    {
+        $this->throwOnAttemptedIteration();
+    }
+
+    private function throwOnAttemptedIteration()
+    {
+
+        throw new DomainException(sprintf(
+            'Attempting to iterate an object (of class %s) which is not iterable.', get_class($this)
+        ));
+    }
+
+}

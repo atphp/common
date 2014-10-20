9dcdd6356991f9fed64a0e83baa6cf8c50c4d405
Andy Truong <thehongtt@gmail.com>
2014-10-20 16:03:46 +0700

    Add meta header.

--- tests/fixtures/Traits/CacheAwareClass.php
+++ tests/fixtures/Traits/CacheAwareClass.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of AT Common package.
+ *
+ * (c) 2014-2014 thehongtt@gmail.com
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace AndyTruong\Common\Fixtures\Traits;
 
 class CacheAwareClass
